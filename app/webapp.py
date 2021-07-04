5e5099616a82d1d28484b5e4fba923f93611a7e9
Hans Maerki <hans.maerki@zhinst.com>
2021-07-04 18:23:28 +0200

    Add log_request

--- app/webapp.py
+++ app/webapp.py
@@ -12,19 +12,50 @@ APP.secret_key = b'_6#y2L"Ftrer\n\xec]/'
 APP.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
 
 
+def log_request(request):
+    APP.logger.warning(f"request.accept_encodings: {request.accept_encodings}")
+    APP.logger.warning(f"request.accept_mimetypes: {request.accept_mimetypes}")
+    APP.logger.warning(f"request.access_route: {request.access_route}")
+    APP.logger.warning(f"request.base_url: {request.base_url}")
+    APP.logger.warning(f"request.charset: {request.charset}")
+    APP.logger.warning(f"request.headers: {request.headers}")
+    APP.logger.warning(f"request.is_json: {request.is_json}")
+    APP.logger.warning(f"request.method: {request.method}")
+    APP.logger.warning(f"request.referrer: {request.referrer}")
+    APP.logger.warning(f"request.remote_addr: {request.remote_addr}")
+    APP.logger.warning(f"request.scheme: {request.scheme}")
+    APP.logger.warning(f"request.url: {request.url}")
+
+
 @APP.route("/server_sent_events_stream")
 def server_sent_events_stream():
-    def generate():
-        for i in range(1000000):
-            time.sleep(0.4)
-            color = ('black', 'black', 'black', 'blue', 'red')[i%5]
-            html = f'<span style="color: {color}">{i},</span>'
-            # html = f'<span>{i}</span>'
-            d = {"html": html }
-            data = f"data: {json.dumps(d)}\n\n"
-            yield data 
+    log_request(flask.request)
 
-    return flask.Response(generate(), mimetype="text/event-stream")
+    def generate():
+        try:
+            APP.logger.warning("generate()")
+            for i in range(20):
+                APP.logger.warning(f"generate(): {i}")
+                time.sleep(0.4)
+                color = ("black", "black", "black", "blue", "red")[i % 5]
+                html = f'<span style="color: {color}">{i},</span>'
+                d = {"html": html}
+                data = f"data: {json.dumps(d)}\n\n"
+                yield data
+        except Exception as e:
+            APP.logger.warning(f"generate() Exception {e}")
+            APP.log_exception(e)
+            raise
+        finally:
+            APP.logger.warning("generate() done")
+
+    return flask.Response(
+        generate(),
+        mimetype="text/event-stream",
+        headers={"Cache-Control": "no-cache",
+        "Connection": "keep-alive"},
+    )
+
 
 @APP.route("/")
 def index():
